485fa125e6f53d8c3c93348a48c2e1bd8c78d913
Eggodjja <s1319941@gmail.com>
2026-05-09 17:45:36 +0200

    added some server part (Network, GameServer, GameClient)

---
+++ core/src/main/java/com/game/Tichet_To_Flight/backend/server/GameServer.java
@@ -0,0 +1,49 @@
+package com.game.Tichet_To_Flight.backend.server;
+
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import com.game.Tichet_To_Flight.network.Network;
+
+import java.io.IOException;
+
+public class GameServer {
+    private Server server;
+
+    public GameServer() {
+        server = new Server();
+
+        // Регистрируем наши пакеты
+        Network.register(server);
+
+        server.addListener(new Listener() {
+            @Override
+            public void connected(Connection connection) {
+                System.out.println("Новый игрок подключился: " + connection.getRemoteAddressTCP());
+            }
+
+            @Override
+            public void received(Connection connection, Object object) {
+                if (object instanceof Network.CreateGameRequest) {
+                    Network.CreateGameRequest req = (Network.CreateGameRequest) object;
+                    System.out.println("Игрок " + req.hostName + " создает игру!");
+
+                    // Отправляем ответ
+                    Network.GameCreatedResponse res = new Network.GameCreatedResponse();
+                    res.success = true;
+                    connection.sendTCP(res);
+                }
+            }
+        });
+    }
+
+    public void start() {
+        try {
+            server.bind(Network.TCP_PORT, Network.UDP_PORT);
+            server.start();
+            System.out.println("Сервер запущен!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
