2665e91067bc25fe78cb1f7639379e72c298cf93
s1319 <s1319941@gmail.com>
2026-05-14 00:25:34 +0200

    added airports, version 1

--- core/src/main/java/com/game/Ticket_To_Flight/packages/PackageInitAirports.java
+++ core/src/main/java/com/game/Ticket_To_Flight/packages/PackageInitAirports.java
@@ -1,7 +1,22 @@
 package com.game.Ticket_To_Flight.packages;
 
+import com.game.Ticket_To_Flight.backend.gameLogicEntities.Airport;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PackageInitAirports {
-    private int i = 0;
+
+    private List<Airport> airports;
+
+    public PackageInitAirports() {
+        this.airports = new ArrayList<>();
+    }
+
+    public PackageInitAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
 }
